fdabadb3b2af3ec918bd5f3858365a070f0d6126
kbyyd24 <melo@gaoyuexiang.cn>
2017-08-27 01:13:27 +0530

    [Yuexiang] add Lambda_libarary

---
+++ src/Lambda_Library.java
@@ -0,0 +1,20 @@
+import java.util.List;
+
+public class Lambda_Library {
+
+    public static void agenda() {}
+
+    public static void map_to_basic_type() {}
+
+    public static void summaryStatistics_method() {}
+
+    // interfaces.Command for example
+    public static void FunctionalInterface_annotation() {}
+
+    public static void default_method() {}
+
+    public static void static_method_in_interface() {}
+
+    public static void optional_type() {}
+
+}
