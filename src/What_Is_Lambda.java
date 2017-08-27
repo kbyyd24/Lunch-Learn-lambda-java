04b5cb0a440c1bc8bda13f39bac514158c0c8b95
kbyyd24 <melo@gaoyuexiang.cn>
2017-08-27 12:00:43 +0530

    [Yuexiang] finish intro method

--- src/What_Is_Lambda.java
+++ src/What_Is_Lambda.java
@@ -2,6 +2,14 @@ public class What_Is_Lambda {
 
     public static void intro() {
 
+        what_is_lambda();
+
+        why_need_lambda();
+
+        what_lambda_look_like();
+
+        some_important_interface();
+
     }
 
     public static void what_is_lambda() {
