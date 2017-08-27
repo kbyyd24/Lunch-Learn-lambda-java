c24644e5d1c47bf91f1acd2f5e753f49572e8f85
kbyyd24 <melo@gaoyuexiang.cn>
2017-08-27 17:03:13 +0530

    [Yuexiang] finish intro method

--- src/Lambda_Library.java
+++ src/Lambda_Library.java
@@ -1,15 +1,33 @@
+import model.TWer;
+
 import java.util.List;
+
+import static java.util.Collections.emptyList;
 
 public class Lambda_Library {
 
-    public static void intro() {}
+    public static void intro() {
+
+        map_to_basic_type();
+
+        summaryStatistics_method(emptyList());
+
+        functionalInterface_annotation();
+
+        default_method();
+
+        static_method_in_interface();
+
+        optional_type();
+
+    }
 
     public static void map_to_basic_type() {}
 
-    public static void summaryStatistics_method() {}
+    public static void summaryStatistics_method(List<TWer> twers) {}
 
     // interfaces.Command for example
-    public static void FunctionalInterface_annotation() {}
+    public static void functionalInterface_annotation() {}
 
     public static void default_method() {}
 
