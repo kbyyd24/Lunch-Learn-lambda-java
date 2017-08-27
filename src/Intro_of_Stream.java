bab644d40947f051671f2fbf2753593b6aa0fa71
kbyyd24 <melo@gaoyuexiang.cn>
2017-08-27 15:09:33 +0530

    [Yuexiang] finish intro method

--- src/Intro_of_Stream.java
+++ src/Intro_of_Stream.java
@@ -1,6 +1,18 @@
 public class Intro_of_Stream {
 
-    public static void intro() {}
+    public static void intro() {
+
+        internal_iteration();
+
+        external_iteration();
+
+        get_stream_from_collection();
+
+        how_stream_works();
+
+        some_options_about_stream();
+
+    }
 
     public static void internal_iteration() {}
 
