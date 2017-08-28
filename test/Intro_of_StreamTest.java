dec832d73a4109bebf854205d257f8d919a7b270
kbyyd24 <melo@gaoyuexiang.cn>
2017-08-28 21:44:08 +0530

    [Yuexiang] update test

--- test/Intro_of_StreamTest.java
+++ test/Intro_of_StreamTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.stream.Stream;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
@@ -24,13 +25,21 @@ public class Intro_of_StreamTest {
 
     @Test
     public void should_print_names_first() {
+
+        Stream<String> nameStream = Stream.of("Bryant", "Jordan", "James")
+                .peek(name -> System.out.println(name))
+                .filter(name -> name.length() == 6);
+
+        System.out.println("count");
+        System.out.println(nameStream.count());
+
+
         String output =
                 "Bryant\n" +
                 "Jordan\n" +
                 "James\n" +
                 "count\n" +
                 "2\n";
-        Intro_of_Stream.how_stream_works();
         assertThat(outputStream.toString(), is(output));
     }
 
