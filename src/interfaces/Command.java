dcb1cf18f0a941025983bba289cab1aa869cb964
kbyyd24 <melo@gaoyuexiang.cn>
2017-09-07 23:34:58 +0530

    [Yuexiang] delete commments

--- src/interfaces/Command.java
+++ src/interfaces/Command.java
@@ -1,15 +1,9 @@
 package interfaces;
 
 @FunctionalInterface
-// A interface which has only one non-overriding abstract method.
-// Won't work when application is running,
-// only use to check if the interface has only one non-overriding abstract method.
 public interface Command {
 
     boolean exec();
-
-//    Multiple non-overriding abstract methods found in interface
-//    String getName();
 
     default String getDescription() {
         return "";
